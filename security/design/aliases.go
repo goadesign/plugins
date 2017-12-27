1c5757d28ff4ff49c2dcb186494b210cc52c8d6b
Nitin Mohan <nitin@rightscale.com>
2017-12-27 13:24:22 -0800

    Regenerate aliases

--- security/design/aliases.go
+++ security/design/aliases.go
@@ -108,122 +108,63 @@ const (
 )
 
 const (
-	// StatusContinue refers to HTTP code 100 (RFC 7231, 6.2.1)
-	StatusContinue = design.StatusContinue
-	// StatusSwitchingProtocols refers to HTTP code 101 (RFC 7231, 6.2.2)
-	StatusSwitchingProtocols = design.StatusSwitchingProtocols
-	// StatusProcessing refers to HTTP code 102 (RFC 2518, 10.1)
-	StatusProcessing = design.StatusProcessing
-	// StatusOK refers to HTTP code 200 (RFC 7231, 6.3.1)
-	StatusOK = design.StatusOK
-	// StatusCreated refers to HTTP code 201 (RFC 7231, 6.3.2)
-	StatusCreated = design.StatusCreated
-	// StatusAccepted refers to HTTP code 202 (RFC 7231, 6.3.3)
-	StatusAccepted = design.StatusAccepted
-	// StatusNonAuthoritativeInfo refers to HTTP code 203 (RFC 7231, 6.3.4)
-	StatusNonAuthoritativeInfo = design.StatusNonAuthoritativeInfo
-	// StatusNoContent refers to HTTP code 204 (RFC 7231, 6.3.5)
-	StatusNoContent = design.StatusNoContent
-	// StatusResetContent refers to HTTP code 205 (RFC 7231, 6.3.6)
-	StatusResetContent = design.StatusResetContent
-	// StatusPartialContent refers to HTTP code 206 (RFC 7233, 4.1)
-	StatusPartialContent = design.StatusPartialContent
-	// StatusMultiStatus refers to HTTP code 207 (RFC 4918, 11.1)
-	StatusMultiStatus = design.StatusMultiStatus
-	// StatusAlreadyReported refers to HTTP code 208 (RFC 5842, 7.1)
-	StatusAlreadyReported = design.StatusAlreadyReported
-	// StatusIMUsed refers to HTTP code 226 (RFC 3229, 10.4.1)
-	StatusIMUsed = design.StatusIMUsed
-	// StatusMultipleChoices refers to HTTP code 300 (RFC 7231, 6.4.1)
-	StatusMultipleChoices = design.StatusMultipleChoices
-	// StatusMovedPermanently refers to HTTP code 301 (RFC 7231, 6.4.2)
-	StatusMovedPermanently = design.StatusMovedPermanently
-	// StatusFound refers to HTTP code 302 (RFC 7231, 6.4.3)
-	StatusFound = design.StatusFound
-	// StatusSeeOther refers to HTTP code 303 (RFC 7231, 6.4.4)
-	StatusSeeOther = design.StatusSeeOther
-	// StatusNotModified refers to HTTP code 304 (RFC 7232, 4.1)
-	StatusNotModified = design.StatusNotModified
-	// StatusUseProxy refers to HTTP code 305 (RFC 7231, 6.4.5)
-	StatusUseProxy = design.StatusUseProxy
-	// StatusTemporaryRedirect refers to HTTP code 307 (RFC 7231, 6.4.7)
-	StatusTemporaryRedirect = design.StatusTemporaryRedirect
-	// StatusPermanentRedirect refers to HTTP code 308 (RFC 7538, 3)
-	StatusPermanentRedirect = design.StatusPermanentRedirect
-	// StatusBadRequest refers to HTTP code 400 (RFC 7231, 6.5.1)
-	StatusBadRequest = design.StatusBadRequest
-	// StatusUnauthorized refers to HTTP code 401 (RFC 7235, 3.1)
-	StatusUnauthorized = design.StatusUnauthorized
-	// StatusPaymentRequired refers to HTTP code 402 (RFC 7231, 6.5.2)
-	StatusPaymentRequired = design.StatusPaymentRequired
-	// StatusForbidden refers to HTTP code 403 (RFC 7231, 6.5.3)
-	StatusForbidden = design.StatusForbidden
-	// StatusNotFound refers to HTTP code 404 (RFC 7231, 6.5.4)
-	StatusNotFound = design.StatusNotFound
-	// StatusMethodNotAllowed refers to HTTP code 405 (RFC 7231, 6.5.5)
-	StatusMethodNotAllowed = design.StatusMethodNotAllowed
-	// StatusNotAcceptable refers to HTTP code 406 (RFC 7231, 6.5.6)
-	StatusNotAcceptable = design.StatusNotAcceptable
-	// StatusProxyAuthRequired refers to HTTP code 407 (RFC 7235, 3.2)
-	StatusProxyAuthRequired = design.StatusProxyAuthRequired
-	// StatusRequestTimeout refers to HTTP code 408 (RFC 7231, 6.5.7)
-	StatusRequestTimeout = design.StatusRequestTimeout
-	// StatusConflict refers to HTTP code 409 (RFC 7231, 6.5.8)
-	StatusConflict = design.StatusConflict
-	// StatusGone refers to HTTP code 410 (RFC 7231, 6.5.9)
-	StatusGone = design.StatusGone
-	// StatusLengthRequired refers to HTTP code 411 (RFC 7231, 6.5.10)
-	StatusLengthRequired = design.StatusLengthRequired
-	// StatusPreconditionFailed refers to HTTP code 412 (RFC 7232, 4.2)
-	StatusPreconditionFailed = design.StatusPreconditionFailed
-	// StatusRequestEntityTooLarge refers to HTTP code 413 (RFC 7231, 6.5.11)
-	StatusRequestEntityTooLarge = design.StatusRequestEntityTooLarge
-	// StatusRequestURITooLong refers to HTTP code 414 (RFC 7231, 6.5.12)
-	StatusRequestURITooLong = design.StatusRequestURITooLong
-	// StatusUnsupportedResultType refers to HTTP code 415 (RFC 7231, 6.5.13)
-	StatusUnsupportedResultType = design.StatusUnsupportedResultType
-	// StatusRequestedRangeNotSatisfiable refers to HTTP code 416 (RFC 7233, 4.4)
-	StatusRequestedRangeNotSatisfiable = design.StatusRequestedRangeNotSatisfiable
-	// StatusExpectationFailed refers to HTTP code 417 (RFC 7231, 6.5.14)
-	StatusExpectationFailed = design.StatusExpectationFailed
-	// StatusTeapot refers to HTTP code 418 (RFC 7168, 2.3.3)
-	StatusTeapot = design.StatusTeapot
-	// StatusUnprocessableEntity refers to HTTP code 422 (RFC 4918, 11.2)
-	StatusUnprocessableEntity = design.StatusUnprocessableEntity
-	// StatusLocked refers to HTTP code 423 (RFC 4918, 11.3)
-	StatusLocked = design.StatusLocked
-	// StatusFailedDependency refers to HTTP code 424 (RFC 4918, 11.4)
-	StatusFailedDependency = design.StatusFailedDependency
-	// StatusUpgradeRequired refers to HTTP code 426 (RFC 7231, 6.5.15)
-	StatusUpgradeRequired = design.StatusUpgradeRequired
-	// StatusPreconditionRequired refers to HTTP code 428 (RFC 6585, 3)
-	StatusPreconditionRequired = design.StatusPreconditionRequired
-	// StatusTooManyRequests refers to HTTP code 429 (RFC 6585, 4)
-	StatusTooManyRequests = design.StatusTooManyRequests
-	// StatusRequestHeaderFieldsTooLarge refers to HTTP code 431 (RFC 6585, 5)
-	StatusRequestHeaderFieldsTooLarge = design.StatusRequestHeaderFieldsTooLarge
-	// StatusUnavailableForLegalReasons refers to HTTP code 451 (RFC 7725, 3)
-	StatusUnavailableForLegalReasons = design.StatusUnavailableForLegalReasons
-	// StatusInternalServerError refers to HTTP code 500 (RFC 7231, 6.6.1)
-	StatusInternalServerError = design.StatusInternalServerError
-	// StatusNotImplemented refers to HTTP code 501 (RFC 7231, 6.6.2)
-	StatusNotImplemented = design.StatusNotImplemented
-	// StatusBadGateway refers to HTTP code 502 (RFC 7231, 6.6.3)
-	StatusBadGateway = design.StatusBadGateway
-	// StatusServiceUnavailable refers to HTTP code 503 (RFC 7231, 6.6.4)
-	StatusServiceUnavailable = design.StatusServiceUnavailable
-	// StatusGatewayTimeout refers to HTTP code 504 (RFC 7231, 6.6.5)
-	StatusGatewayTimeout = design.StatusGatewayTimeout
-	// StatusHTTPVersionNotSupported refers to HTTP code 505 (RFC 7231, 6.6.6)
-	StatusHTTPVersionNotSupported = design.StatusHTTPVersionNotSupported
-	// StatusVariantAlsoNegotiates refers to HTTP code 506 (RFC 2295, 8.1)
-	StatusVariantAlsoNegotiates = design.StatusVariantAlsoNegotiates
-	// StatusInsufficientStorage refers to HTTP code 507 (RFC 4918, 11.5)
-	StatusInsufficientStorage = design.StatusInsufficientStorage
-	// StatusLoopDetected refers to HTTP code 508 (RFC 5842, 7.2)
-	StatusLoopDetected = design.StatusLoopDetected
-	// StatusNotExtended refers to HTTP code 510 (RFC 2774, 7)
-	StatusNotExtended = design.StatusNotExtended
-	// StatusNetworkAuthenticationRequired refers to HTTP code 511 (RFC 6585, 6)
+	StatusContinue                      = design.StatusContinue
+	StatusSwitchingProtocols            = design.StatusSwitchingProtocols
+	StatusProcessing                    = design.StatusProcessing
+	StatusOK                            = design.StatusOK
+	StatusCreated                       = design.StatusCreated
+	StatusAccepted                      = design.StatusAccepted
+	StatusNonAuthoritativeInfo          = design.StatusNonAuthoritativeInfo
+	StatusNoContent                     = design.StatusNoContent
+	StatusResetContent                  = design.StatusResetContent
+	StatusPartialContent                = design.StatusPartialContent
+	StatusMultiStatus                   = design.StatusMultiStatus
+	StatusAlreadyReported               = design.StatusAlreadyReported
+	StatusIMUsed                        = design.StatusIMUsed
+	StatusMultipleChoices               = design.StatusMultipleChoices
+	StatusMovedPermanently              = design.StatusMovedPermanently
+	StatusFound                         = design.StatusFound
+	StatusSeeOther                      = design.StatusSeeOther
+	StatusNotModified                   = design.StatusNotModified
+	StatusUseProxy                      = design.StatusUseProxy
+	StatusTemporaryRedirect             = design.StatusTemporaryRedirect
+	StatusPermanentRedirect             = design.StatusPermanentRedirect
+	StatusBadRequest                    = design.StatusBadRequest
+	StatusUnauthorized                  = design.StatusUnauthorized
+	StatusPaymentRequired               = design.StatusPaymentRequired
+	StatusForbidden                     = design.StatusForbidden
+	StatusNotFound                      = design.StatusNotFound
+	StatusMethodNotAllowed              = design.StatusMethodNotAllowed
+	StatusNotAcceptable                 = design.StatusNotAcceptable
+	StatusProxyAuthRequired             = design.StatusProxyAuthRequired
+	StatusRequestTimeout                = design.StatusRequestTimeout
+	StatusConflict                      = design.StatusConflict
+	StatusGone                          = design.StatusGone
+	StatusLengthRequired                = design.StatusLengthRequired
+	StatusPreconditionFailed            = design.StatusPreconditionFailed
+	StatusRequestEntityTooLarge         = design.StatusRequestEntityTooLarge
+	StatusRequestURITooLong             = design.StatusRequestURITooLong
+	StatusUnsupportedResultType         = design.StatusUnsupportedResultType
+	StatusRequestedRangeNotSatisfiable  = design.StatusRequestedRangeNotSatisfiable
+	StatusExpectationFailed             = design.StatusExpectationFailed
+	StatusTeapot                        = design.StatusTeapot
+	StatusUnprocessableEntity           = design.StatusUnprocessableEntity
+	StatusLocked                        = design.StatusLocked
+	StatusFailedDependency              = design.StatusFailedDependency
+	StatusUpgradeRequired               = design.StatusUpgradeRequired
+	StatusPreconditionRequired          = design.StatusPreconditionRequired
+	StatusTooManyRequests               = design.StatusTooManyRequests
+	StatusRequestHeaderFieldsTooLarge   = design.StatusRequestHeaderFieldsTooLarge
+	StatusUnavailableForLegalReasons    = design.StatusUnavailableForLegalReasons
+	StatusInternalServerError           = design.StatusInternalServerError
+	StatusNotImplemented                = design.StatusNotImplemented
+	StatusBadGateway                    = design.StatusBadGateway
+	StatusServiceUnavailable            = design.StatusServiceUnavailable
+	StatusGatewayTimeout                = design.StatusGatewayTimeout
+	StatusHTTPVersionNotSupported       = design.StatusHTTPVersionNotSupported
+	StatusVariantAlsoNegotiates         = design.StatusVariantAlsoNegotiates
+	StatusInsufficientStorage           = design.StatusInsufficientStorage
+	StatusLoopDetected                  = design.StatusLoopDetected
+	StatusNotExtended                   = design.StatusNotExtended
 	StatusNetworkAuthenticationRequired = design.StatusNetworkAuthenticationRequired
 )
