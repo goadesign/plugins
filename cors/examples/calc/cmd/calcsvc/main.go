3bd142a06adf1032c80ed3a66d27684d7c1db9dd
Raphael Simon <simon.raphael@gmail.com>
2018-03-12 15:39:10 -0700

    Update examples with latest goa v2 logger interface

--- cors/examples/calc/cmd/calcsvc/main.go
+++ cors/examples/calc/cmd/calcsvc/main.go
@@ -32,11 +32,11 @@ func main() {
 	// packages define log adapters for common log packages.
 	var (
 		logger  *log.Logger
-		adapter logging.Adapter
+		adapter logging.Logger
 	)
 	{
 		logger = log.New(os.Stderr, "[calc] ", log.Ltime)
-		adapter = logging.Adapt(logger)
+		adapter = logging.NewLogger(logger)
 	}
 
 	// Create the structs that implement the services.
@@ -113,9 +113,9 @@ func main() {
 	srv := &http.Server{Addr: *addr, Handler: handler}
 	go func() {
 		for _, m := range calcsvcServer.Mounts {
-			logger.Printf("[INFO] service %q method %q mounted on %s %s", calcsvcServer.Service(), m.Method, m.Verb, m.Pattern)
+			logger.Printf("[calc] service %q method %q mounted on %s %s", calcsvcServer.Service(), m.Method, m.Verb, m.Pattern)
 		}
-		logger.Printf("[INFO] listening on %s", *addr)
+		logger.Printf("[calc] listening on %s", *addr)
 		errc <- srv.ListenAndServe()
 	}()
 
