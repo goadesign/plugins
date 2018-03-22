c4151e3abe77f8529edc2cf1877c5018ff7873f2
Louis Cloutier <louis.l.cloutier@gmail.com>
2018-03-22 16:19:15 -0400

    Update error type from interface{}

--- goakit/encode_decode_files.go
+++ goakit/encode_decode_files.go
@@ -137,8 +137,8 @@ const responseEncoderT = `{{ printf "%s returns a go-kit EncodeResponseFunc suit
 const errorEncoderT = `{{ printf "%s returns a go-kit EncodeResponseFunc suitable for encoding errors returned by the %s %s endpoint." .ErrorEncoder .ServiceName .Method.Name | comment }}
  func {{ .ErrorEncoder }}(encoder func(context.Context, http.ResponseWriter) goahttp.Encoder) kithttp.ErrorEncoder {
  	enc := server.{{ .ErrorEncoder }}(encoder)
-	return func(ctx context.Context, v interface{}, w http.ResponseWriter) error {
-		enc(ctx, w, v.(error))
+	return func(ctx context.Context, err error, w http.ResponseWriter) error {
+		enc(ctx, w, err)
 		return nil
 	}
 }
