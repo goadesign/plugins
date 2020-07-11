a951369f3bb122a1b6ae72f1055ecafdb91d3733
Raphael Simon <simon.raphael@gmail.com>
2020-07-11 13:07:00 -0700

    Release v3.2.0

--- goakit/examples/fetcher/archiver/cmd/archiver/main.go
+++ goakit/examples/fetcher/archiver/cmd/archiver/main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 	"strings"
 	"sync"
+	"syscall"
 
 	"github.com/go-kit/kit/log"
 	archiverapi "goa.design/plugins/v3/goakit/examples/fetcher/archiver"
@@ -67,7 +68,7 @@ func main() {
 	// that SIGINT and SIGTERM signals cause the services to stop gracefully.
 	go func() {
 		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 		errc <- fmt.Errorf("%s", <-c)
 	}()
 
