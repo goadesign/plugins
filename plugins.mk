a6f3c593d7c6b4d2ad60ccc85f51f2f73825a91d
Taichi Sasaki <tchsskk@gmail.com>
2026-05-05 23:26:04 +0900

    Ignore *.pb.go from goimports

--- plugins.mk
+++ plugins.mk
@@ -21,7 +21,7 @@ test:
 	@go test ./...
 
 lint:
-	$(eval GO_FILES := $(shell find . -type f -name '*.go'))
+	$(eval GO_FILES := $(shell find . -type f -name '*.go' ! -name '*.pb.go'))
 	@if [ "`goimports -l $(GO_FILES) | tee /dev/stderr`" ]; then \
 		echo "^ - Repo contains improperly formatted go files" && echo && exit 1; \
 	fi
